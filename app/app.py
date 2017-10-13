9696eb7ec64a17379be541549aba08f543ccb734
johannah <jh1736@gmail.com>
2017-10-13 11:34:13 -0400

    working on tablet

--- app/app.py
+++ app/app.py
@@ -2,9 +2,6 @@ import os
 
 from flask import Flask, render_template, request, jsonify
 
-from pyimagesearch.colordescriptor import ColorDescriptor
-from pyimagesearch.searcher import Searcher
-
 # create flask instance
 app = Flask(__name__)
 
@@ -28,31 +25,13 @@ def search():
         image_url = request.form.get('img')
 
         try:
-
-            # initialize the image descriptor
-            cd = ColorDescriptor((8, 12, 3))
-
-            # load the query image and describe it
-            from skimage import io
-            query = io.imread(image_url)
-            features = cd.describe(query)
-
-            # perform the search
-            searcher = Searcher(INDEX)
-            results = searcher.search(features)
-
-            # loop over the results, displaying the score and image name
-            for (score, resultID) in results:
-                RESULTS_ARRAY.append(
-                    {"image": str(resultID), "score": str(score)})
-
-            # return success
-            return jsonify(results=(RESULTS_ARRAY[::-1][:3]))
-
+            ## return success
+            print(image_url)
+            return jsonify(results=({"image":image_url}))
+            #return jsonify(results=(RESULTS_ARRAY[::-1][:3]))
         except:
-
             # return error
-            jsonify({"sorry": "Sorry, no results! Please try again."}), 500
+            return jsonify({"sorry": "Sorry, no results! Please try again."}), 500
 
 
 # run!
